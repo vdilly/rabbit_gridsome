49b9eb46c5b39dad3a5922ed80fc0fa3f3ad8e74
Vincent Dilly <vdilly.prod@gmail.com>
2019-12-29 16:38:53 +0100

    maj env

--- src/main.js
+++ src/main.js
@@ -65,8 +65,20 @@ export default function(Vue, { app, router, head, isClient, appOptions }) {
   Vue.mixin({
     methods: {
       parseWpUrl: function(string) {
-        const wpurl = new RegExp(process.env.GRIDSOME_WP_URL, "g");
-        return string.replace(wpurl, process.env.GRIDSOME_SITE_URL);
+        const urls = [
+          process.env.GRIDSOME_WP_LOCAL_URL,
+          process.env.GRIDSOME_WP_PERSO_URL,
+          process.env.GRIDSOME_WP_PREPROD_URL,
+          process.env.GRIDSOME_WP_PROD_URL
+        ];
+
+        urls.forEach(url => {
+          if (!url) return;
+          const wpurl = new RegExp(url, "g");
+          string = string.replace(wpurl, process.env.GRIDSOME_SITE_URL);
+        });
+
+        return string;
       }
     },
     computed: {
