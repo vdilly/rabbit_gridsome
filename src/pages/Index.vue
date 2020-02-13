3951899e3e3d336d07ff1b471f61760285368c4a
vdilly <vdilly.prod@gmail.com>
2020-02-13 14:48:15 +0100

    pugify home

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,21 @@
-<template>
-  <Layout :isFront="true">
-    <Banner v-slot="banner" :pageTitle="home.title"></Banner>
-    <Container>
-      <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-      <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-        excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-        Dolores, aperiam non officia eos quod asperiores
-      </p>
-
-      <p class="home-links">
-        <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
-          >Gridsome Docs</a
-        >
-        <a
-          href="https://github.com/gridsome/gridsome"
-          target="_blank"
-          rel="noopener"
-          >GitHub</a
-        >
-      </p>
-      <img
-        lazy="img"
+<template lang="pug">
+  Layout(:isFront="true")
+    Banner(v-slot="banner" :pageTitle="home.title")
+    Container
+      //- Learn how to use images here: https://gridsome.org/docs/images 
+      g-image(alt="Example image" src="~/favicon.png" width="135")
+      p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      p.home-links
+        a(href="https://gridsome.org/docs/" target="_blank" rel="noopener") Gridsome Docs
+        a(href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener") Github
+      img(lazy="img"
         lazy-src="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
         src="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=32&w=63"
         height="1260"
         width="750"
-        alt
-      />
-    </Container>
-    <SwipeSample></SwipeSample>
-    <ParaSlideSample></ParaSlideSample>
-  </Layout>
+        alt)
+    SwipeSample
+    ParaSlideSample
 </template>
 
 <script>
