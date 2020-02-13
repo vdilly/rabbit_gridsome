ce75662d47e97aad67ea34a727e5364fc3c41be0
vdilly <vdilly.prod@gmail.com>
2020-02-13 11:43:39 +0100

    feat (slugify) : remove accents

--- src/js/plugins/slugify.js
+++ src/js/plugins/slugify.js
@@ -2,6 +2,8 @@ export default function(text) {
   return text
     .toString()
     .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
     .replace(/\s+/g, "-") // Replace spaces with -
     .replace(/[^\w\-]+/g, "") // Remove all non-word chars
     .replace(/\-\-+/g, "-") // Replace multiple - with single -
