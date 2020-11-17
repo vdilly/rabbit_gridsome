c085db2518ed065d2a5308f5465b9a333d1507cb
vdilly <vdilly.prod@gmail.com>
2020-11-17 18:07:57 +0100

    fix system string

--- src/js/plugins/systemString.js
+++ src/js/plugins/systemString.js
@@ -6,8 +6,9 @@ export default function(string) {
   const p = new RegExp(a.split("").join("|"), "g");
   string = string
     .trim()
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
     .replace(/\s{2,}/g, " ") // Remove multiple spaces
-    .replace(/\s+/g, "_"); // Replace spaces with _
+    .replace(/\s+/g, "_") // Replace spaces with _
+    .replace(/\?/g, ""); // Replace spaces with _
   return string;
 }
