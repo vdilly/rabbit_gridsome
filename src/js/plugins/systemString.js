155657f57f2db19bd3af52a00c4f9618606f33f0
vdilly <vdilly.prod@gmail.com>
2020-11-17 18:50:11 +0100

    recup system string Celine

--- src/js/plugins/systemString.js
+++ src/js/plugins/systemString.js
@@ -5,10 +5,13 @@ export default function(string) {
     "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------";
   const p = new RegExp(a.split("").join("|"), "g");
   string = string
+    .toLowerCase()
     .trim()
     .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/\./g, "") // Remove multiple spaces
     .replace(/\s{2,}/g, " ") // Remove multiple spaces
+    .trim()
     .replace(/\s+/g, "_") // Replace spaces with _
-    .replace(/\?/g, ""); // Replace spaces with _
+    .replace(/\?/g, ""); // Remove question marks
   return string;
 }
