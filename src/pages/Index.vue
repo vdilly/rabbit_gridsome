e693f4f366a0e4a0fc1b0b56e4de8ceb4c9a7c76
Vincent Dilly <vdilly.prod@gmail.com>
2021-09-30 17:22:52 +0200

    setup

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,56 @@
 <template lang="pug">
-  Layout(:isFront="true")
-    Banner(slot="banner" :pageTitle="home.title")
-    Container
-      p(js-observe-scroll scroll-anim="y8")
-        img(
-          lazy="img"
-          lazy-src="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-          src="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=32&w=63"
-          height="1260"
-          width="750"
-          alt
-        )
-      //- Learn how to use images here: https://gridsome.org/docs/images 
-      g-image(alt="Example image" src="~/favicon.png" width="135" js-observe-scroll scroll-anim="scale0")
-      p(js-observe-scroll scroll-anim="x8") Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-      p.home-links(js-observe-scroll)
-        a(href="https://gridsome.org/docs/" target="_blank" rel="noopener" scroll-anim="y8" style="display: inline-block;") Gridsome Docs
-        a(href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener" scroll-anim="y8" scroll-delay="1" style="display: inline-block;") Github
-    Swipe1
-    ParaSlide
+Layout(:isFront="true")
+  Banner(slot="banner", :pageTitle="home.title")
+  Container
+    ul
+      li
+        p Item 1
+        button.snipcart-add-item(
+          data-item-id="starry-night",
+          data-item-price="79.99",
+          data-item-url="/starry-night",
+          data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh.",
+          data-item-name="The Starry Night",
+          data-item-image="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+          data-item-custom1-name="Frame color",
+          data-item-custom1-options="Black|Brown|Gold"
+        ) Add to cart
+    p(js-observe-scroll, scroll-anim="y8")
+      img(
+        lazy="img",
+        lazy-src="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        src="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=32&w=63",
+        height="1260",
+        width="750",
+        alt
+      )
+    //- Learn how to use images here: https://gridsome.org/docs/images 
+    g-image(
+      alt="Example image",
+      src="~/favicon.png",
+      width="135",
+      js-observe-scroll,
+      scroll-anim="scale0"
+    )
+    p(js-observe-scroll, scroll-anim="x8") Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    p.home-links(js-observe-scroll)
+      a(
+        href="https://gridsome.org/docs/",
+        target="_blank",
+        rel="noopener",
+        scroll-anim="y8",
+        style="display: inline-block"
+      ) Gridsome Docs
+      a(
+        href="https://github.com/gridsome/gridsome",
+        target="_blank",
+        rel="noopener",
+        scroll-anim="y8",
+        scroll-delay="1",
+        style="display: inline-block"
+      ) Github
+  Swipe1
+  ParaSlide
 </template>
 
 <script>
@@ -29,7 +61,7 @@ export default {
   mixins: [pageMixin],
   components: {
     Swipe1,
-    ParaSlide
+    ParaSlide,
   },
   computed: {
     home() {
@@ -41,15 +73,15 @@ export default {
         this.home.title,
         this.home.content
       );
-    }
+    },
   },
   metaInfo() {
     return {
       title: this.seo.title,
       link: this.seo.link,
-      meta: this.seo.meta
+      meta: this.seo.meta,
     };
-  }
+  },
 };
 </script>
 
---
+++ src/pages/Starry-night.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div
+  p Item 1
+  button.snipcart-add-item(
+    data-item-id="starry-night",
+    data-item-price="79.99",
+    data-item-url="/starry-night",
+    data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh.",
+    data-item-name="The Starry Night",
+    data-item-image="https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    data-item-custom1-name="Frame color",
+    data-item-custom1-options="Black|Brown|Gold"
+  ) Add to cart
+</template>
